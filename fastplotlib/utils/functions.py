9ccff906c7f7fe2acb376fda68919f1988b34df5
Flynn OConnell <flynnoconnell@gmail.com>
2026-09-18 16:55:20 -0500

    coolwarm

--- fastplotlib/utils/functions.py
+++ fastplotlib/utils/functions.py
@@ -61,7 +61,7 @@ COLORMAPS = sorted(
         "RdYlBu",
         "RdYlGn",
         "Spectral",
-        "coolwarm",
+        "matplotlib:coolwarm",
         "bwr",
         "seismic",
         "berlin",
